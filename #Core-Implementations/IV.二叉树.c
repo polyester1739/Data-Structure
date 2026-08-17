91d08575b13809240ad13902ec5f9a629e68ee07
LIU SHUOCHEN <sliu0292@student.monash.edu>
2026-08-17 16:24:33 +0800

    Made some adjustments.

--- #Core-Implementations/IV.二叉树.c
+++ #Core-Implementations/IV.二叉树.c
@@ -21,8 +21,7 @@ int maxDepth(TreeNode* root);
 int findDiameter(TreeNode* root);
 void deleteTree(TreeNode* root);
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]) {
 	//测试用例
 	int values[12] = { 1,2,3,4,5,INT_MIN,8,INT_MIN,INT_MIN,6,7,9 };
 	int* valueList = (int*)malloc(sizeof(int) * 12);
@@ -37,6 +36,7 @@ int main(int argc, const char* argv[])
 	printf("\n");
 	levelOrderTraversal(root, numberOfNode(root));
 	printf("\n");
+	printf("Number of nodes: %d\n", numberOfNode(root));
 	printf("Number of leaves: %d\n", numberOfLeaf(root));
 	printf("Min depth: %d\n", minDepth(root, numberOfNode(root)));
 	printf("Max depth: %d\n", maxDepth(root));
@@ -44,8 +44,7 @@ int main(int argc, const char* argv[])
 	deleteTree(root);
 }
 
-TreeNode* createTreeNode(int val)
-{
+TreeNode* createTreeNode(int val) {
 	TreeNode* tree = (TreeNode*)malloc(sizeof(TreeNode));
 	if (!tree) exit(EXIT_FAILURE);
 	tree->val = val;
@@ -54,168 +53,140 @@ TreeNode* createTreeNode(int val)
 	return tree;
 }
 
-TreeNode* createBinaryTree(int* valuesList, int listLength)
-{
+TreeNode* createBinaryTree(int* valuesList, int listLength) {
 	if (!valuesList || listLength < 1 || valuesList[0] == INT_MIN) return NULL;
 
-	TreeNode** nodes = (TreeNode**)calloc(listLength, sizeof(TreeNode*));
-	if (!nodes) exit(EXIT_FAILURE);
+	TreeNode** queue = (TreeNode**)calloc(listLength, sizeof(TreeNode*));
+	if (!queue) exit(EXIT_FAILURE);
 
 	int rear = 0, front = 0;
-	nodes[rear] = createTreeNode(valuesList[rear]);
-	rear++;
-	while (rear < listLength && front < rear)
-	{
-		int size = rear - front;
-		for (int i = 0; i < size; i++)
-		{
-			if (valuesList[front] == INT_MIN) front++;
-			else
-			{
-				TreeNode* cur = nodes[front++];
-				if (rear >= listLength) break;
-				if (valuesList[rear] != INT_MIN)
-				{
-					nodes[rear] = createTreeNode(valuesList[rear]);
-					cur->left = nodes[rear];
-					rear++;
-				}
-				else rear++;
-				if (rear >= listLength) break;
-				if (rear < listLength && valuesList[rear] != INT_MIN)
-				{
-					nodes[rear] = createTreeNode(valuesList[rear]);
-					cur->right = nodes[rear];
-					rear++;
-				}
-				else rear++;
+	queue[rear++] = createTreeNode(valuesList[0]);
+	int p = 1;
+
+	while (p < listLength && front < rear) {
+		TreeNode* cur = queue[front++];
+
+		if (p < listLength) {
+			if (valuesList[p] != INT_MIN) {
+				cur->left = createTreeNode(valuesList[p]);
+				queue[rear++] = cur->left;
 			}
+			p++;
+		}
+
+		if (p < listLength) {
+			if (valuesList[p] != INT_MIN) {
+				cur->right = createTreeNode(valuesList[p]);
+				queue[rear++] = cur->right;
+			}
+			p++;
 		}
 	}
-	TreeNode* root = nodes[0];
-	free(nodes);
+
+	TreeNode* root = queue[0];
+	free(queue);
 	return root;
 }
 
-void preOrderTraversal(TreeNode* root)
-{
+void preOrderTraversal(TreeNode* root) {
 	if (root == NULL) return;
-	else
-	{
+	else {
 		printf("%d\t", root->val);
 		preOrderTraversal(root->left);
 		preOrderTraversal(root->right);
 	}
 }
 
-void inOrderTraversal(TreeNode* root)
-{
+void inOrderTraversal(TreeNode* root) {
 	if (root == NULL) return;
-	else
-	{
+	else {
 		inOrderTraversal(root->left);
 		printf("%d\t", root->val);
 		inOrderTraversal(root->right);
 	}
 }
 
-void postOrderTraversal(TreeNode* root)
-{
+void postOrderTraversal(TreeNode* root) {
 	if (root == NULL) return;
-	else
-	{
+	else {
 		postOrderTraversal(root->left);
 		postOrderTraversal(root->right);
 		printf("%d\t", root->val);
 	}
 }
 
-void levelOrderTraversal(TreeNode* root, int sizeOfTree)
-{
+void levelOrderTraversal(TreeNode* root, int sizeOfTree) {
 	int rear = 0, front = 0;
-	TreeNode** nodes = (TreeNode**)malloc(sizeof(TreeNode*) * sizeOfTree);
-	if (!nodes) exit(EXIT_FAILURE);
-	nodes[rear++] = root;
-	while (front < rear)
-	{
+	TreeNode** queue = (TreeNode**)malloc(sizeof(TreeNode*) * sizeOfTree);
+	if (!queue) exit(EXIT_FAILURE);
+	queue[rear++] = root;
+	while (front < rear) {
 		int size = rear - front;
-		for (int i = 0; i < size; i++)
-		{
-			TreeNode* cur = nodes[front++];
-			if (cur->left != NULL) nodes[rear++] = cur->left;
-			if (cur->right != NULL) nodes[rear++] = cur->right;
+		for (int i = 0; i < size; i++) {
+			TreeNode* cur = queue[front++];
+			if (cur->left != NULL) queue[rear++] = cur->left;
+			if (cur->right != NULL) queue[rear++] = cur->right;
 		}
 	}
-	for (int i = 0; i < sizeOfTree; i++) printf("%d\t", nodes[i]->val);
-	free(nodes);
+	for (int i = 0; i < sizeOfTree; i++) printf("%d\t", queue[i]->val);
+	free(queue);
 }
 
-int numberOfNode(TreeNode* root)
-{
+int numberOfNode(TreeNode* root) {
 	if (root == NULL) return 0;
-	else
-	{
+	else {
 		int left = numberOfNode(root->left);
 		int right = numberOfNode(root->right);
 		return 1 + left + right;
 	}
 }
 
-int numberOfLeaf(TreeNode* root)
-{
+int numberOfLeaf(TreeNode* root) {
 	if (root == NULL) return 0;
 	if (root->left == NULL && root->right == NULL) return 1;
-	else
-	{
+	else {
 		int leftLeaf = numberOfLeaf(root->left);
 		int rightLeaf = numberOfLeaf(root->right);
 		return leftLeaf + rightLeaf;
 	}
 }
 
-int minDepth(TreeNode* root, int sizeOfTree)
-{
+int minDepth(TreeNode* root, int sizeOfTree) {
 	if (root == NULL) return 0;
-	int rear = 0, front = 0, depth = 1;
-	TreeNode** nodes = (TreeNode**)malloc(sizeof(TreeNode*) * sizeOfTree);
-	if (!nodes) exit(EXIT_FAILURE);
-	nodes[rear++] = root;
-	while (front < rear)
-	{
+	int rear = 0, front = 0, depth = 0;
+	TreeNode** queue = (TreeNode**)malloc(sizeof(TreeNode*) * sizeOfTree);
+	if (!queue) exit(EXIT_FAILURE);
+	queue[rear++] = root;
+
+	while (front < rear) {
 		int size = rear - front;
-		for (int i = 0; i < size; i++)
-		{
-			TreeNode* cur = nodes[front++];
-			if (cur->left == NULL && cur->right == NULL)
-			{
-				free(nodes);
+		depth++;
+		for (int i = 0; i < size; i++) {
+			TreeNode* cur = queue[front++];
+			if (cur->left == NULL && cur->right == NULL) {
+				free(queue);
 				return depth;
 			}
-			if (cur->left != NULL) nodes[rear++] = cur->left;
-			if (cur->right != NULL) nodes[rear++] = cur->right;
+			if (cur->left != NULL) queue[rear++] = cur->left;
+			if (cur->right != NULL) queue[rear++] = cur->right;
 		}
-		depth++;
 	}
-	free(nodes);
+	free(queue);
 	return depth;
 }
 
-int maxDepth(TreeNode* root)
-{
+int maxDepth(TreeNode* root) {
 	if (root == NULL) return 0;
-	else
-	{
+	else {
 		int leftMax = maxDepth(root->left);
 		int rightMax = maxDepth(root->right);
 		return 1 + (leftMax > rightMax ? leftMax : rightMax);
 	}
 }
 
-int findPath(TreeNode* root, int* maxLength)
-{
+int findPath(TreeNode* root, int* maxLength) {
 	if (root == NULL) return 0;
-	else
-	{
+	else {
 		int leftPath = findPath(root->left, maxLength);
 		int rightPath = findPath(root->right, maxLength);
 		if (leftPath + rightPath > *maxLength) *maxLength = leftPath + rightPath;
@@ -223,18 +194,15 @@ int findPath(TreeNode* root, int* maxLength)
 	}
 }
 
-int findDiameter(TreeNode* root)
-{
+int findDiameter(TreeNode* root) {
 	int maxLength = 0;
 	findPath(root, &maxLength);
 	return maxLength;
 }
 
-void deleteTree(TreeNode* root)
-{
+void deleteTree(TreeNode* root) {
 	if (root == NULL) return;
-	else
-	{
+	else {
 		deleteTree(root->left);
 		deleteTree(root->right);
 		free(root);
